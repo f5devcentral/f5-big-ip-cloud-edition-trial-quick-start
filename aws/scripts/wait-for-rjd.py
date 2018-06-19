3265a5c80099f2fdae2899688fd25bf005ca0073
Kyle Housley <k.housley@f5.com>
2018-06-19 11:14:20 -0700

    kill ssl warnings when waiting for rjd ready

--- aws/scripts/wait-for-rjd.py
+++ aws/scripts/wait-for-rjd.py
@@ -1,7 +1,10 @@
 #! /usr/local/bin/python2.7
 import util
+import logger
+import urllib3
 
 def main():
+    util.kill_ssl_warnings(logger, urllib3)
     util.poll_for_services_available("localhost", None, timeout=1200)
 
 if __name__ == '__main__':
