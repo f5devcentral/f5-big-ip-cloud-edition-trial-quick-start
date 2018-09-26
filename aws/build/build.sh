6b52e13baf1a0e5ff5ec3c64e60d2159c78ec646
Roman Jouhannet <r.jouhannet@f5.com>
2018-09-25 20:29:57 -0700

    update 6.0.1 HF1

--- aws/build/build.sh
+++ aws/build/build.sh
@@ -2,18 +2,18 @@
 mkdir ../built
 # Pack up scripts into an archive
 tar cvz ../scripts/* > ../built/scripts.tar.gz
-fname="bigiq-cm-dcd-pair-with-ssg.template"
+fname="bigiq-cm-dcd-pair-with-ssg.template-pm"
 output_dir="../experimental/"
 template_output="$output_dir$fname"
 # Get name of current branch, ideally it includes the BIQ version number
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 # Upload scripts archive
-aws s3 cp --acl public-read ../built/scripts.tar.gz "s3://big-iq-quickstart-cf-templates/$branch_name/"
+aws s3 cp --acl public-read ../built/scripts.tar.gz "s3://big-iq-quickstart-cf-templates-pm/$branch_name/"
 
 # Compile template file
 ./big-iq-master.py --branch $branch_name > $template_output
 
 # Copy templates
 for f in ../experimental/*.template; do
-    aws s3 cp --acl public-read "$f" "s3://big-iq-quickstart-cf-templates/$branch_name/"
+    aws s3 cp --acl public-read "$f" "s3://big-iq-quickstart-cf-templates-pm/$branch_name/"
 done
