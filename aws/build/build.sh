dc657f3c55a556a6a67720fe1b797ec75fd41572
Roman <r.jouhannet@f5.com>
2019-05-08 00:28:34 -0700

    update 5/7

--- aws/build/build.sh
+++ aws/build/build.sh
@@ -9,16 +9,16 @@ template_output="$output_dir$fname"
 # Get name of current branch, ideally it includes the BIQ version number
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 # Upload scripts archive
-aws s3 cp --acl public-read ../built/scripts.tar.gz "s3://big-iq-quickstart-cf-templates-aws/$branch_name/"
+aws s3 cp --acl public-read ../built/scripts.tar.gz "s3://big-ip-ce-quickstart-cf-templates-aws/$branch_name/"
 
 # Compile template file
 #./big-iq-master.py --branch $branch_name > $template_output
 
 # Copy templates
 for f in ../experimental/*.template; do
-    aws s3 cp --acl public-read "$f" "s3://big-iq-quickstart-cf-templates-aws/$branch_name/"
+    aws s3 cp --acl public-read "$f" "s3://big-ip-ce-quickstart-cf-templates-aws/$branch_name/"
 done
 
-#aws s3 cp --acl public-read aws/experimental/Setup-Ubuntu-Trial.template "s3://big-iq-quickstart-cf-templates-aws/6.0.1.1/"
-#aws s3 cp --acl public-read aws/experimental/bigiq-cm-dcd-pair-with-ssg.template "s3://big-iq-quickstart-cf-templates-aws/6.0.1.1/"
-#aws s3 cp --acl public-read aws/experimental/bigiq-cm-dcd-pair-with-ssg-existing-vpc.template "s3://big-iq-quickstart-cf-templates-aws/6.0.1.1/"
+#aws s3 cp --acl public-read aws/experimental/Setup-Ubuntu-Trial.template "s3://big-ip-ce-quickstart-cf-templates-aws/6.1.0/"
+#aws s3 cp --acl public-read aws/experimental/bigiq-cm-dcd-pair-with-ssg.template "s3://big-ip-ce-quickstart-cf-templates-aws/6.1.0/"
+#aws s3 cp --acl public-read aws/experimental/bigiq-cm-dcd-pair-with-ssg-existing-vpc.template "s3://big-ip-ce-quickstart-cf-templates-aws/6.1.0/"
